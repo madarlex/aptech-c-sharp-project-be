c2a19b7dc51e5e2ee25b660f9c31acb56369dca8
Madarlex <madarlex@gmail.com>
2022-03-14 20:36:33 +0700

    set up

---
+++ MobileRecharge/MobileRecharge/Program.cs
@@ -0,0 +1,21 @@
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionStrings = builder.Configuration["ConnectionStrings:DefaultConnection"];
+builder.Services.AddControllers();
+
+builder.Services.AddCors();
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed((host) => true).AllowCredentials());
+
+app.MapControllers();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.Run();
