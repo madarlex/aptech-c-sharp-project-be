d6df3a61a5a5f9cbc2c418bd3e891a53eabb5124
Madarlex <madarlex@gmail.com>
2022-03-23 18:56:30 +0700

    recharge

--- MobileRecharge/MobileRecharge/Program.cs
+++ MobileRecharge/MobileRecharge/Program.cs
@@ -1,21 +1,28 @@
 
+using Microsoft.EntityFrameworkCore;
+using MobileRecharge.Models;
+using MobileRecharge.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionStrings = builder.Configuration["ConnectionStrings:DefaultConnection"];
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
 
+    
 builder.Services.AddCors();
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddScoped<PrepaidService, PrepaidServiceImpl>();
+builder.Services.AddDbContext<DatabaseContext>(option => option.UseLazyLoadingProxies().UseSqlServer(connectionStrings));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed((host) => true).AllowCredentials());
 
-app.MapControllers();
-
 app.UseStaticFiles();
 
 app.UseRouting();
+app.MapControllers();
 
 app.Run();
